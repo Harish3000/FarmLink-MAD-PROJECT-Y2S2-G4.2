fcdac10760ab156e5e446268c58fd1c3411cad1c
IT21269516 <it21269516@my.sliit.lk>
2023-04-21 20:35:51 +0530

    Address Data Insert
    Successful

---
+++ app/src/main/java/com/example/farmlink/AddressData.kt
@@ -0,0 +1,11 @@
+package com.example.farmlink
+
+data class AddressData(
+    var addressId: String? = null,
+    var fullName : String? = null,
+    var addressLine1 : String? = null,
+    var addressLine2 : String? = null,
+    var city : String? = null,
+    var district : String? = null,
+    var phone : String? = null,
+)
