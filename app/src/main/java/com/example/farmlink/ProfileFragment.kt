7ca453387a2f35791f3868495ee002e35749d54f
Harini chamathka <harinichamathka14@gmail.com>
2023-04-23 15:06:01 +0530

    Update Profile Fragment (Customer)

--- app/src/main/java/com/example/farmlink/ProfileFragment.kt
+++ app/src/main/java/com/example/farmlink/ProfileFragment.kt
@@ -1,10 +1,14 @@
 package com.example.farmlink
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,13 +33,13 @@ class ProfileFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_profile, container, false)
+//    }
 
     companion object {
         /**
@@ -56,4 +60,18 @@ class ProfileFragment : Fragment() {
                 }
             }
     }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val button = view.findViewById<Button>(R.id.my_button)
+        button.setOnClickListener {
+            val intent = Intent(activity, CustomerRegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
+    }
 }
+
+
