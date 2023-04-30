fb31a2fe4049cbba3dd43a8d7b1ca9b3e97a9393
Harini chamathka <harinichamathka14@gmail.com>
2023-04-30 07:31:31 +0530

    change files

--- app/src/main/java/com/example/farmlink/CustomerRegisterActivity.kt
+++ app/src/main/java/com/example/farmlink/CustomerRegisterActivity.kt
@@ -1,11 +1,33 @@
 package com.example.farmlink
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.farmlink.databinding.ActivityCustomerRegisterBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class CustomerRegisterActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityCustomerRegisterBinding
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_customer_register)
+        binding = ActivityCustomerRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.Signup.setOnClickListener{
+            val name =binding.Name.text.toString()
+            val email=binding.email.text.toString()
+            val username =binding.username.text.toString()
+            val password =binding.Password.text.toString()
+
+
+            // Write a message to the database
+            // Write a message to the database
+            var database = FirebaseDatabase.getInstance().getReference("Customer")
+
+            myRef.setValue("Hello, World!")
+
+        }
     }
 }
