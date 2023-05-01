05de8c8732427b195e74473710817f07de9ad23f
Harini chamathka <harinichamathka14@gmail.com>
2023-05-01 11:52:49 +0530

    update

--- app/src/main/java/com/example/farmlink/CustomerRegisterActivity.kt
+++ app/src/main/java/com/example/farmlink/CustomerRegisterActivity.kt
@@ -9,35 +9,45 @@ import com.google.firebase.database.FirebaseDatabase
 
 class CustomerRegisterActivity : AppCompatActivity() {
 
-
+    private lateinit var  binding: ActivityCustomerRegisterBinding
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_customer_register)
+        binding = ActivityCustomerRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.Signup.setOnClickListener{
+            val name =binding.Name.text.toString()
+            val email=binding.email.text.toString()
+            val username =binding.username.text.toString()
+            val password =binding.Password.text.toString()
 
 
+            var database  =FirebaseDatabase.getInstance().getReference("Customer")
+            val customer = customer(name,email,username,password)
+            database.child(username).setValue(customer).addOnSuccessListener {
 
-            var firebase :DatabaseReference =FirebaseDatabase.getInstance().getReference("customer")
-//            val customer = customer(name,email,username,password)
-//            firebase.child(username).setValue(customer).addOnSuccessListener {
-//
-//                binding.Name.text.clear()
-//                binding.email.text.clear()
-//                binding.username.text.clear()
-//                binding.Password.text.clear()
-//
-//                Toast.makeText(this,"Sucessfully saved",Toast.LENGTH_SHORT).show()
-//            }.addOnFailureListener{
-//                Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
-//            }
+                binding.Name.text.clear()
+                binding.email.text.clear()
+                binding.username.text.clear()
+                binding.Password.text.clear()
+
+                Toast.makeText(this,"Sucessfully saved",Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener{
+                Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
+            }
 
         }
     }
+
+
+
 
     private fun customer(name: String, email: String, username: String, password: String): Any? {
         TODO("Not yet implemented")
     }
 }
 
-private fun CharSequence.clear() {
+private fun Any.clear() {
     TODO("Not yet implemented")
 }
