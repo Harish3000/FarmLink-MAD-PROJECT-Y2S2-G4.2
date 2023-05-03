317866f74cadb1c2723aad9dda228a56cb4caadc
Rehantp <rehamc123@gmail.com>
2023-05-03 12:00:38 +0530

    class

---
+++ app/src/main/java/com/example/farmlink/itemDs.kt
@@ -0,0 +1,5 @@
+package com.example.farmlink
+
+import android.provider.Telephony.Mms.Rate
+
+data class itemDs(val itemName:String?=null,val itemPrice:String?=null,val itemImg:String?="",val description:String?=null)
