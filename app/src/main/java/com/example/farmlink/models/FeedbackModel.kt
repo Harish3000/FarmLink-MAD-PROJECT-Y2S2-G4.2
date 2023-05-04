36f91e84193c9ececcc596836adc37068ef3e7b5
Harish <129611646+Harish3000@users.noreply.github.com>
2023-05-04 19:53:41 +0530

    updated fragments

---
+++ app/src/main/java/com/example/farmlink/models/FeedbackModel.kt
@@ -0,0 +1,8 @@
+package com.example.farmlink.models
+
+class FeedbackModel (
+    var userId: String? = null,
+    var userName: String? = null,
+    var email: String? = null,
+    var description: String? = null
+        )
