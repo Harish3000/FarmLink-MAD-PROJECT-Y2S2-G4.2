5ceef4d13d98911912600303e4b0ac43c29f080b
Harini chamathka <harinichamathka14@gmail.com>
2023-05-04 14:47:45 +0530

    update files

--- app/src/main/java/com/example/farmlink/CustomerSignUpPage.kt
+++ app/src/main/java/com/example/farmlink/CustomerSignUpPage.kt
@@ -1,13 +1,20 @@
 package com.example.farmlink
 
 import android.annotation.SuppressLint
+import android.app.Dialog
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class CustomerSignUpPage : AppCompatActivity() {
 
@@ -16,10 +23,9 @@ class CustomerSignUpPage : AppCompatActivity() {
     private lateinit var etCusUsername :EditText
     private lateinit var etCusPassword :EditText
     private lateinit var btnSignUp :Button
-
     private lateinit var dbRef:DatabaseReference
+    private lateinit var dialog: Dialog
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_sign_up_page)
@@ -30,11 +36,15 @@ class CustomerSignUpPage : AppCompatActivity() {
         etCusPassword =findViewById(R.id.enterthePassword)
         btnSignUp =findViewById(R.id.SignupBtn)
 
+        //get the reference from customer node
         dbRef =FirebaseDatabase.getInstance().getReference("Customers")
 
         btnSignUp.setOnClickListener {
+            showProgressBar()
             saveCustomerData()
         }
+
+
     }
 
     private fun saveCustomerData(){
@@ -60,25 +70,40 @@ class CustomerSignUpPage : AppCompatActivity() {
 
         //create unique ID
         val cusId =dbRef.push().key!!
-
         val customer =CustomerModel(cusId,cusName,cusEmail,cusUsername,cusPassword)
 
         //send the data to firebase database
         dbRef.child(cusId).setValue(customer)  //create child using the cusId
             .addOnCompleteListener{
-                Toast.makeText(this,"Registered Successfully..",Toast.LENGTH_LONG).show()
+                if(it.isSuccessful) {
 
-                //clear the data after register to the system
-                etCusName.text.clear()
-                etCusEmail.text.clear()
-                etCusUsername.text.clear()
-                etCusPassword.text.clear()
+                    Toast.makeText(this, "Registered Successfully..", Toast.LENGTH_LONG).show()
 
+                    //clear the data after register to the system
+                    etCusName.text.clear()
+                    etCusEmail.text.clear()
+                    etCusUsername.text.clear()
+                    etCusPassword.text.clear()
+                }
             }.addOnFailureListener{err ->
+                hideProgressBar()
                 Toast.makeText(this,"Couldn't register!! Error ${err.message}",Toast.LENGTH_LONG).show()
 
             }
 
     }
 
+
+
+    private fun showProgressBar(){
+        dialog=Dialog(this@CustomerSignUpPage)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_wait)
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.show()
+    }
+
+    private fun hideProgressBar(){
+        dialog.dismiss()
+    }
 }
