a4e39eec59206342e75793ee0b2bc9d6aa99c431
Harish <129611646+Harish3000@users.noreply.github.com>
2023-05-04 19:20:31 +0530

    implemented product page

---
+++ app/src/main/java/com/example/farmlink/activities/ProductDetails.kt
@@ -0,0 +1,46 @@
+package com.example.farmlink.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.farmlink.models.FeedbackModel
+import com.example.farmlink.R
+import com.google.firebase.database.FirebaseDatabase
+
+class ProductDetails : AppCompatActivity() {
+    private lateinit var tvSellerId: TextView
+    private lateinit var tvSellerName: TextView
+    private lateinit var tvRating: TextView
+    private lateinit var tvDescription: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_details)
+        initView()
+        setValuesToViews()
+
+    }
+
+
+    private fun initView() {
+        tvSellerId = findViewById(R.id.tvSellerId)
+        tvSellerName = findViewById(R.id.tvSellerName)
+        tvRating = findViewById(R.id.tvRating)
+        tvDescription = findViewById(R.id.tvDescription)
+
+    }
+
+    private fun setValuesToViews() {
+        tvSellerId.text = intent.getStringExtra("sellerId")
+        tvSellerName.text = intent.getStringExtra("sellerName")
+        tvRating.text = intent.getStringExtra("rating")
+        tvDescription.text = intent.getStringExtra("description")
+
+    }
+}
