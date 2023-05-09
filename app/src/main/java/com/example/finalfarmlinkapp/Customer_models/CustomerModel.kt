10c7382afcb09fa6923d3971499b92cb0c045121
Harini chamathka <harinichamathka14@gmail.com>
2023-05-09 19:10:05 +0530

    updated file

---
+++ app/src/main/java/com/example/finalfarmlinkapp/Customer_models/CustomerModel.kt
@@ -0,0 +1,10 @@
+package com.example.finalfarmlinkapp.Customer_models
+
+data class CustomerModel(
+    var cusId:String?=null,
+    var cusName: String?=null,
+    var cusEmail:String?=null,
+    var cusUsername:String?=null,
+    var cusPassword:String?=null
+
+)
---
+++ app/src/main/java/com/example/finalfarmlinkapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.finalfarmlinkapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHomeFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHomeFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        setupWithNavController(bottomNavigationView, navController)
+    }
+
+    val firebaseApp :DatabaseReference=FirebaseDatabase.getInstance().getReference()
+    }
+
+
+
+
+
