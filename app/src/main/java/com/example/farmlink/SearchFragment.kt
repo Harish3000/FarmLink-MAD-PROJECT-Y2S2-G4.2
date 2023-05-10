e7ad84985095023e87ad511129ac3c93df1d3258
Harish <129611646+Harish3000@users.noreply.github.com>
2023-05-10 11:39:25 +0530

    implementing search function

--- app/src/main/java/com/example/farmlink/SearchFragment.kt
+++ app/src/main/java/com/example/farmlink/SearchFragment.kt
@@ -1,59 +1,88 @@
 package com.example.farmlink
-
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.*
+import com.example.farmlink.store_manager_adapter.FeedbackAdapter
+import com.example.farmlink.store_manager_models.FeedbackModel
+import com.example.farmlink.store_manager_activities.FeedbackDetails
+import com.example.farmlink.store_manager_activities.ProductDetails
+import com.example.farmlink.store_manager_adapter.SellerAdapter
+import com.example.farmlink.store_manager_models.SellerModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var sellerRecyclerView: RecyclerView
+    private lateinit var tvLoadingData: TextView
+    private lateinit var sellerList: ArrayList<SellerModel>
+    private lateinit var dbRef: DatabaseReference
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
+
+        sellerRecyclerView = view.findViewById(R.id.rvSeller)
+        sellerRecyclerView.layoutManager = LinearLayoutManager(activity)
+        sellerRecyclerView.setHasFixedSize(true)
+        tvLoadingData = view.findViewById(R.id.tvLoadingData)
+
+        sellerList = arrayListOf<SellerModel>()
+
+        getSellerData()
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun getSellerData() {
+
+        sellerRecyclerView.visibility = View.GONE
+        tvLoadingData.visibility = View.VISIBLE
+
+        dbRef = FirebaseDatabase.getInstance().getReference("Seller")
+
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                sellerList.clear()
+                if (snapshot.exists()){
+                    for (sellerSnap in snapshot.children){
+                        val sellerData = sellerSnap.getValue(SellerModel::class.java)
+                        sellerList.add(sellerData!!)
+                    }
+                    val mAdapter = SellerAdapter(sellerList)
+                    sellerRecyclerView.adapter = mAdapter
+
+                    mAdapter.setOnItemClickListener(object : SellerAdapter.onItemClickListener {
+                        override fun onItemClick(position: Int) {
+
+                            val intent = Intent(activity, ProductDetails::class.java)
+
+                            //put extras
+                            intent.putExtra("sellerId", sellerList[position].sellerId)
+                            intent.putExtra("sellerName", sellerList[position].sellerName)
+                            intent.putExtra("rating", sellerList[position].rating)
+                            intent.putExtra("description", sellerList[position].description)
+                            startActivity(intent)
+                        }
+
+                    })
+
+                    sellerRecyclerView.visibility = View.VISIBLE
+                    tvLoadingData.visibility = View.GONE
                 }
             }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle error
+            }
+
+        })
     }
 }
